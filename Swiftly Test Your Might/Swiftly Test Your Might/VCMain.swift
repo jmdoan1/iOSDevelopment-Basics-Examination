bc3a5a99875f9e8f4c5718af61acea8de2c7b7b9
Justin Doan <justindoan@justindoan.com>
2018-04-15 18:06:17 -0400

    banana: Added collectionView subview to magenta view with all constraints 0, set cell width (and for now, height) equal to collectionView's, set bottom inset and minimum spacings to 3. As this is my first time with collection views, I'll be testing the size and spacing once data is loaded

--- Swiftly Test Your Might/Swiftly Test Your Might/VCMain.swift
+++ Swiftly Test Your Might/Swiftly Test Your Might/VCMain.swift
@@ -11,8 +11,61 @@ import UIKit
 /// Hey, have fun!
 class VCMain: UIViewController
 {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    fileprivate let edgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 3, right: 0)
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
     }
+}
+
+extension VCMain: UICollectionViewDataSource, UICollectionViewDelegate
+{
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "reuseIdentifier",
+                                                      for: indexPath)
+        cell.backgroundColor = UIColor.black
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 2
+    }
+}
+
+extension VCMain: UICollectionViewDelegateFlowLayout
+{
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        return CGSize(width: self.collectionView.frame.width, height: self.collectionView.frame.width)
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        
+        return edgeInsets
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return edgeInsets.bottom
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return edgeInsets.bottom
+    }
+    
 }
